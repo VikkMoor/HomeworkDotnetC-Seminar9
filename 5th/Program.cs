0138d37d495582bc20faf4fca673da1478a7e1ee
Vikk_Moor <victimofuroboros@gmail.com>
2022-12-15 21:16:58 +0500

    5th

---
+++ 5th/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter A: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter B: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int Step(int A, int B)
+{
+    if (B == 0) return 1;
+    return A * Step(A, B - 1);
+}
+
+Console.Write(Step(a, b));
