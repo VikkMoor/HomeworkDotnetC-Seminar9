1aadd6e30e2bbb80b390c1530bdc5a2ef476f25b
Vikk_Moor <victimofuroboros@gmail.com>
2022-12-15 20:59:30 +0500

    3rdReady

---
+++ 3rd/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Enter non-negative M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter non-negative N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 0 || m < 0) Console.WriteLine("Enter correct numbers!");
+int Accerman(int M, int N)
+{
+    if (M == 0 && N > -1) return N + 1;
+    else if (M > 0 && N == 0) return Accerman(M - 1, 1);
+    else if (M > 0 && N > 0) return Accerman(M - 1, Accerman(M, N - 1));
+    else return 0;
+}
+int k = Accerman(m, n);
+Console.Write(k);
