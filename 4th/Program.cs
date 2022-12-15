6e462367619d293eacb19c55dc35293d221a3812
Vikk_Moor <victimofuroboros@gmail.com>
2022-12-15 21:15:23 +0500

    4th

---
+++ 4th/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Enter number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+ int Sum(int N)
+{
+    if (N % 10 == 0) return N;
+    return (N % 10) + Sum(N / 10);
+}
+
+Console.Write(Sum(num));
