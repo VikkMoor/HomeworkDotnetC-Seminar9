8fdfb039ef26c9042fbdda025762c6bc6fc00ec1
Victoria Moor <115675892+VikkMoor@users.noreply.github.com>
2022-12-16 13:15:27 +0500

    Update Program.cs

--- 3rd/Program.cs
+++ 3rd/Program.cs
@@ -3,12 +3,12 @@ int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Enter non-negative N: ");
 int n = Convert.ToInt32(Console.ReadLine());
 if (n < 0 || m < 0) Console.WriteLine("Enter correct numbers!");
-int Accerman(int M, int N)
+int Ackermann(int M, int N)
 {
     if (M == 0 && N > -1) return N + 1;
-    else if (M > 0 && N == 0) return Accerman(M - 1, 1);
-    else if (M > 0 && N > 0) return Accerman(M - 1, Accerman(M, N - 1));
+    else if (M > 0 && N == 0) return Ackermann(M - 1, 1);
+    else if (M > 0 && N > 0) return Ackermann(M - 1, Ackermann(M, N - 1));
     else return 0;
 }
-int k = Accerman(m, n);
+int k = Ackermann(m, n);
 Console.Write(k);
